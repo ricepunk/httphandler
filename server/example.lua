9328b12b6a947cc5bc932fc36e1f4a324be13ade
ricepunk <23333938+ricepunk@users.noreply.github.com>
2024-08-20 08:33:55 +0700

    refactor: full rework

--- server/example.lua
+++ server/example.lua
@@ -6,23 +6,46 @@ for i = 1, 100 do
   players[i] = {
     id = i,
     name = 'Player ' .. i,
-    money = math.random(1, 1000),
-    coords = { x = math.random(1, 1000), y = math.random(1, 1000), z = math.random(1, 1000) }
+    money = math.random(1, 100000)
   }
 end
 
-router.get('/player', function(req, res)
-  res.json(players)
+router:setAuth(function(req, res)
+  local ip = req.address:match("([^:]+)")
+  return ip == '127.0.0.1' or req.headers.Authorization == 'Basic 123456789'
 end)
 
-router.get('/player/:id', function(req, res)
-  local id = tonumber(req.params.id)
+router:get('/players', function(req, res)
+  res.sendJson(players)
+end)
 
-  if not players[id] then
+router:get('/players/(%d+)', function(req, res)
+  local player = players[tonumber(req.params[1])]
+  if not player then
     res.writeHead(404)
-    res.send('Player with id ' .. id .. ' not found')
+    res.send('Player not found')
     return
   end
 
-  res.json(players[id])
+  res.sendJson(players[tonumber(req.params[1])])
+end)
+
+router:put('/players', function(req, res)
+  local body = req.readBody()
+  if not body then
+    res.writeHead(400)
+    res.send('Missing body')
+    return
+  end
+
+  body = json.decode(body)
+
+  local count = #players
+  players[count + 1] = {
+    id = count + 1,
+    name = body.name or ('Player %s'):format(count + 1),
+    money = math.random(1, 100000)
+  }
+
+  res.sendJson(players[count + 1])
 end)
--- server/router.lua
+++ server/router.lua
@@ -1,93 +1,67 @@
-if not IsDuplicityVersion() then
-  lib.print.error('Cannot load router module in client environment')
-  return {}
-end
-
 ---@class HTTPRequest
---- @field path string
---- @field method 'GET'|'POST'|'PUT'|'DELETE'
---- @field address string
---- @field headers table<string,string>
---- @field setDataHandler fun(handler:fun(body: string):nil):nil
---- @field setCancelHandler fun(handler:fun():nil):nil
---- @field params? { [string]: any }
+---@field address string the IP address of the request sender
+---@field headers table<string, string> the headers of the request
+---@field method 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' the request method.
+---@field path string the path to where the request was sent
+---@field params? table<string, string> the query parameters of the request
+---@field setDataHandler fun(handler: fun(body: string)) sets the handler for when the request is finished
+---@field setCancelHandler fun(handler: fun()) sets the handler for when the request is finished
+---@field readBody fun(): string reads the body of the request
 
 ---@class HTTPResponse
----@field writeHead fun(statusCode: number, headers?: { [string]: string })
----@field write fun(data: string)
----@field send fun(data?: string)
----@field json fun(data: table)
+---@field writeHead fun(code: number, headers?: table<string, string>) sets the status code & headers of the response. Can be only called once and won't work if called after running other response functions.
+---@field write fun(data: string) writes to the response body without sending it. Can be called multiple times.
+---@field send fun(data?: string) writes to the response body and then sends it along with the status code & headers, finishing the request
+---@field sendJson fun(data: string | table, code?: number) sends a json response along with the status code & headers, finishing the request
 
 ---@class Router : OxClass
 local Router = lib.class('Router')
-local config = require 'config'
-local APIKEY = config.APIKEY
-local HOST = config.HOST
 
----@param path string
----@param url string
+---@param reqPath string
+---@param routePath string
 ---@return boolean
 ---@return table?
-local function matchRoute(path, url)
-  local paramNames = {}
+local function matchPath(reqPath, routePath)
+  -- Check for an exact match
+  reqPath = reqPath:gsub('/$', '')
+  if reqPath == routePath then
+    return true
+  end
 
-  local pattern = '^' .. path:gsub('(:%w+)', function(param)
-    local paramName = param:sub(2)
-    table.insert(paramNames, paramName)
-    return '([^/]+)'
-  end) .. '$'
-
-  local matches = { url:match(pattern) }
-
-  local params = {}
-  if #matches > 0 then
-    for i, paramName in ipairs(paramNames) do
-      params[paramName] = matches[i]
-    end
-
+  -- Check for a pattern match
+  local pattern = '^' .. routePath .. '$'
+  local params = { reqPath:match(pattern) }
+  if #params > 0 then
     return true, params
   end
 
   return false
 end
 
----@param res HTTPResponse
----@param status number
----@param message? string
-local function cancelResponse(res, status, message)
-  res.writeHead(status or 400)
-  res.send(message or 'Bad request')
-end
-
 function Router:constructor()
-  local routes = lib.array:new()
+  self.routes = lib.array:new()
 
   ---@param req HTTPRequest
   ---@param res HTTPResponse
-  ---@return nil
   SetHttpHandler(function(req, res)
-    if HOST then
-      if req.address:match('([^:]+)') ~= HOST then
-        return cancelResponse(res, 403)
+    if self.auth then
+      local success = self.auth(req, res)
+      if not success then
+        res.writeHead(401)
+        res.send('Unauthorized')
+        return
       end
     end
 
-    if APIKEY then
-      local apiKey = req.headers['x-api-key']
-      if apiKey ~= APIKEY then
-        return cancelResponse(res, 401)
-      end
-    end
+    local route = self.routes:find(function(route)
+      if req.method ~= route.method then return false end
 
-    local route = routes:find(function(route)
-      if route.method ~= req.method then return false end
-      local isMatch, params = matchRoute(route.path, req.path)
-
-      if isMatch then
+      local matched, params = matchPath(req.path, route.path)
+      if params then
         req.params = params
       end
 
-      return isMatch
+      return matched
     end)
 
     if not route then
@@ -96,41 +70,53 @@ function Router:constructor()
       return
     end
 
-    res.json = function(data)
-      res.writeHead(200, { ['Content-Type'] = 'application/json' })
-      res.send(json.encode(data))
+    req.readBody = function()
+      local p = promise.new()
+      if req.headers['Content-Length'] == '0' then
+        p:resolve()
+      end
+
+      req.setDataHandler(function(body)
+        p:resolve(body)
+      end)
+
+      req.setCancelHandler(function()
+        p:reject()
+      end)
+
+      return Citizen.Await(p)
     end
 
-    local success, result = pcall(route.callback, req, res)
+    res.sendJson = function(data, code)
+      ---@cast data string
+      data = type(data) == 'table' and json.encode(data) or data
+      res.writeHead(code or 200, { ['Content-Type'] = 'application/json' })
+      res.send(data)
+    end
+
+    local success, result = pcall(route.cb, req, res)
     if not success then
       res.writeHead(500)
-      res.send(result or 'Internal server error')
+      res.send(result or 'Internal Server Error')
     end
   end)
+end
 
-  ---@param path string
-  ---@param callback fun(req: HTTPRequest, res: HTTPResponse)
-  self.get = function(path, callback)
-    routes:push({ method = 'GET', path = path, callback = callback })
-  end
+---@param cb fun(request: HTTPRequest, response: HTTPResponse): boolean
+function Router:setAuth(cb)
+  self.auth = cb
+end
 
-  ---@param path string
-  ---@param callback fun(req: HTTPRequest, res: HTTPResponse)
-  self.put = function(path, callback)
-    routes:push({ method = 'PUT', path = path, callback = callback })
-  end
+---@param path string
+---@param cb fun(request: HTTPRequest, response: HTTPResponse)
+function Router:get(path, cb)
+  self.routes:push({ method = 'GET', path = path, cb = cb })
+end
 
-  ---@param path string
-  ---@param callback fun(req: HTTPRequest, res: HTTPResponse)
-  self.post = function(path, callback)
-    routes:push({ method = 'POST', path = path, callback = callback })
-  end
-
-  ---@param path string
-  ---@param callback fun(req: HTTPRequest, res: HTTPResponse)
-  self.delete = function(path, callback)
-    routes:push({ method = 'DELETE', path = path, callback = callback })
-  end
+---@param path string
+---@param cb fun(request: HTTPRequest, response: HTTPResponse)
+function Router:put(path, cb)
+  self.routes:push({ method = 'PUT', path = path, cb = cb })
 end
 
 return Router:new()
